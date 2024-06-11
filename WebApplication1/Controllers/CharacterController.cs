651abeac2982277959a793839deabcd7052f6d32
s27973 <s27973@pjwstk.edu.pl>
2024-06-11 13:04:13 +0200

    commit dto

---
+++ WebApplication1/Controllers/CharacterController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CharacterController : ControllerBase
+{
+    private readonly IDbService _dbService;
+    public CharacterController(IDbService dbService)
+    {
+        _dbService = dbService;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetCharacterData(string? clientLastName = null)
+    {
+        var orders = await _dbService.GetOrdersData(clientLastName);
+        
+        return Ok(orders.Select(e => new GetOrdersDTO()
+        {
+            Id = e.Id,
+            AcceptedAt = e.AcceptedAt,
+            FulfilledAt = e.FulfilledAt,
+            Comments = e.Comments,
+            Pastries = e.OrderPastries.Select(p => new GetOrdersPastryDTO
+            {
+                Name = p.Pastry.Name,
+                Price = p.Pastry.Price,
+                Amount = p.Amount
+            }).ToList()
+        }));
+    }
+}
---
+++ WebApplication1/DTOs/GetCharacterDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.DTOs;
+
+public class GetCharacterDTO
+{
+    public int Id { get; set; }
+    public String FirstName { get; set; } = null!;
+    public String LastName { get; set; } = null!;
+    private int CurrentWei { get; set; }
+    private int MaxWeight { get; set; }
+}
+
+public class GetBackpackDTO
+{
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+    public int Amount { get; set; }
+}
+
+public class GetCharacter_titleDTO
+{
+    public DateTime AcquiredAt { get; set; }
+}
