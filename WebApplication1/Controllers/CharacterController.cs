1bf33c0e6c222bd4edcb8119046decdcffdee8f7
s27973 <s27973@pjwstk.edu.pl>
2024-06-11 13:16:37 +0200

    commit

--- WebApplication1/Controllers/CharacterController.cs
+++ WebApplication1/Controllers/CharacterController.cs
@@ -12,4 +12,24 @@ public class CharacterController : ControllerBase
     {
         _dbService = dbService;
     }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetCharacterData(int characterId)
+    {
+        var clients = await _dbService.GetOrdersData(characterId);
+        
+        return Ok(orders.Select(e => new GetOrdersDTO()
+        {
+            Id = e.Id,
+            AcceptedAt = e.AcceptedAt,
+            FulfilledAt = e.FulfilledAt,
+            Comments = e.Comments,
+            Pastries = e.OrderPastries.Select(p => new GetOrdersPastryDTO
+            {
+                Name = p.Pastry.Name,
+                Price = p.Pastry.Price,
+                Amount = p.Amount
+            }).ToList()
+        }));
+    }
 }
--- WebApplication1/Services/DbService.cs
+++ WebApplication1/Services/DbService.cs
@@ -1,6 +1,22 @@
-﻿namespace WebApplication1.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services;
 
 public class DbService: IDbService
 {
+    
+    private readonly DatabaseContext _context;
+    public DbService(DatabaseContext context)
+    {
+        _context = context;
+    }
+    public async Task<ICollection<Character>> GetCharactersData(int characterId)
+    {
+        return await _context.Characters
+            .Where(e => clientLastName == null || e.ch.LastName == clientLastName)
+            .ToListAsync();
+    }
     
 }
